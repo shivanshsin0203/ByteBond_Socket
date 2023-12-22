2b27597aaab640dafc70a9734aa5305d6409f738
Shivansh Singh <singhshivansh12may@gmail.com>
2023-12-22 11:10:39 +0530

    Adding socket server

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const httpServer = http.createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["http://localhost:3000",],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected ' + socket.id);
+
+  socket.on('join_room', (data) => {
+    console.log("joid id"+data)
+    console.log("joining room " + data.roomid)
+    socket.join(data.roomid);
+    const room = io.sockets.adapter.rooms.get(data.roomid);
+   
+  });
+
+  socket.on('data_send', (data) => {
+    console.log(data);
+    io.to(data.roomid).emit('data_receive', data);
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+
+httpServer.listen(PORT, () => {
+  console.log(`Server started at ${PORT}`);
+});
