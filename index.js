8a63a98f5148891fdf170f66330dd6c2f9803cea
Shivansh Singh <singhshivansh12may@gmail.com>
2023-12-22 11:45:52 +0530

    Added Chat Feature

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// server.js
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
@@ -7,21 +8,18 @@ const httpServer = http.createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:3000",],
-    allowedHeaders: ["my-custom-header"],
-    credentials: true
-  }
+    origin: ['http://localhost:3000'],
+    allowedHeaders: ['my-custom-header'],
+    credentials: true,
+  },
 });
 
 io.on('connection', (socket) => {
   console.log('a user connected ' + socket.id);
 
   socket.on('join_room', (data) => {
-    console.log("joid id"+data)
-    console.log("joining room " + data.roomid)
+    console.log('joining room ' + data.roomid);
     socket.join(data.roomid);
-    const room = io.sockets.adapter.rooms.get(data.roomid);
-   
   });
 
   socket.on('data_send', (data) => {
