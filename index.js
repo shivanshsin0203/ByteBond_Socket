e699b39e2185ad57f01087246ec171b51f24ef94
Shivansh Singh <singhshivansh12may@gmail.com>
2023-12-22 22:57:37 +0530

    Added online and typing

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const mongoose = require('mongoose');
-const dotenv=require('dotenv');
+const dotenv = require('dotenv');
 dotenv.config();
-const mongoUrl=process.env.mongoUrl;
+const mongoUrl = process.env.mongoUrl;
 const app = express();
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
@@ -17,18 +17,17 @@ const io = new Server(httpServer, {
 
 mongoose.connect(mongoUrl);
 const messageSchema = new mongoose.Schema(
-    {
-      roomid: String,
-      sender: String,
-      message: String,
-    },
-    {
-      timestamps: true,
-    }
-  );
-  
-  const Message = mongoose.model('Message', messageSchema);
-  
+  {
+    roomid: String,
+    sender: String,
+    message: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Message = mongoose.model('Message', messageSchema);
 
 io.on('connection', (socket) => {
   console.log('a user connected ' + socket.id);
@@ -38,7 +37,7 @@ io.on('connection', (socket) => {
     socket.join(data.roomid);
     const room = io.sockets.adapter.rooms.get(data.roomid);
     io.to(data.roomid).emit('online', {
-        size: room.size,
+      size: room.size,
     });
   });
 
@@ -47,6 +46,22 @@ io.on('connection', (socket) => {
     io.to(data.roomid).emit('data_receive', data);
     const newMessage = new Message(data);
     newMessage.save();
+  });
+
+  socket.on('typing', (data) => {
+    io.to(data.roomid).emit('typing_recived', data);
+  });
+
+  socket.on('disconnect', () => {
+    const rooms = Array.from(socket.rooms);
+    rooms.forEach((room) => {
+      const currentRoom = io.sockets.adapter.rooms.get(room);
+      if (currentRoom) {
+        io.to(room).emit('online', {
+          size: currentRoom.size,
+        });
+      }
+    });
   });
 });
 
